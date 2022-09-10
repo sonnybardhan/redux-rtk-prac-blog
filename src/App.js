8401ea0d0f8ff955d56b4de6179a3c9def15e1c9
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-09-10 16:13:09 +0530

    initial clean up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
-import Counter from './features/counter/Counter';
 
 function App() {
-  return (
-    <div className='App'>
-      <Counter />
-    </div>
-  );
+  return <div className='App'>hello </div>;
 }
 
 export default App;
